56a08949f3418cb29fd930389b41cb0d0a7f5438
Rob Golding <rob@robgolding.com>
2015-09-23 15:56:28 +0100

    Add docker and aws modules

---
+++ modules/aws/init.zsh
@@ -0,0 +1,4 @@
+function awsenv() {
+   export AWS_DEFAULT_PROFILE=$1
+   export AWS_PROFILE=$1
+}
--- modules/docker/init.zsh
+++ modules/docker/init.zsh
@@ -2,4 +2,8 @@
 # Provides handy aliases for docker and docker-compose
 #
 
+$(boot2docker shellinit 2>/dev/null)
+
+alias b2d='boot2docker'
+alias b2di='$(boot2docker shellinit)'
 alias dc='docker-compose'
